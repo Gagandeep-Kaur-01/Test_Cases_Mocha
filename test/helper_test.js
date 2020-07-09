81fef7000ad970620415f1f5a55316cd286c48c3
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-07-09 14:32:49 +0530

    Used beforeEach in mocha tests

--- test/helper_test.js
+++ test/helper_test.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise; //ES6 - Promise
 
+before((done) =>  {
 mongoose.connect("mongodb://localhost/mongotube", {useNewUrlParser: true});
 
 mongoose.connection
-   .once("open", ()  => console.log('connected'))
+   .once("open", ()  =>  {
+   // console.log('connected'))
+   done();
+})
+
    .on("error", (error) => {
        console.log("Your error", error);
    });
-   
+   });
    //once and on are listeners to the events, whatever the event is happening, whatever successful, failure, etc event be.
    
